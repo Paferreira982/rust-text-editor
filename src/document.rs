f394e86f15d9d09ec78ad6e14295f8354e187133
Pedro Almeida <paferreira982@hotmail.com>
2024-01-07 17:04:11 -0300

    perf: :zap: improve perfomance when highlighting
    improved perfomance by not rehilighting non edited rows

--- src/document.rs
+++ src/document.rs
@@ -82,6 +82,14 @@ impl Document {
             let row = &mut self.rows[at.y];
             row.insert(at.x, c);       
         }
+        self.unhighlight_rows(at.y);
+    }
+
+    fn unhighlight_rows(&mut self, start: usize) {
+        let start = start.saturating_sub(1);
+        for row in self.rows.iter_mut().skip(start) {
+            row.is_highlighted = false;
+        }
     }
 
     #[allow(clippy::integer_arithmetic, clippy::indexing_slicing)]
@@ -102,6 +110,8 @@ impl Document {
             let row = &mut self.rows[at.y];
             row.delete(at.x);
         }
+
+        self.unhighlight_rows(at.y);
     }
 
     pub fn save(&mut self) -> Result<(), Error> {
--- src/row.rs
+++ src/row.rs
@@ -9,6 +9,7 @@ use unicode_segmentation::UnicodeSegmentation;
 pub struct Row {
     string: String,
     highlighting: Vec<highlighting::Type>,
+    pub is_highlighted: bool,
     len: usize,
 }
 
@@ -17,6 +18,7 @@ impl From<&str> for Row {
         Self {
             string: String::from(slice),
             highlighting: Vec::new(),
+            is_highlighted: false,
             len: slice.graphemes(true).count(),
         }
     }
@@ -137,9 +139,11 @@ impl Row {
 
         self.string = row;
         self.len = length;
+        self.is_highlighted = false;
         Self {
             string: splitted_row,
             len: splitted_length,
+            is_highlighted: false,
             highlighting: Vec::new(),
         }
     }
@@ -440,8 +444,20 @@ impl Row {
         word: &Option<String>,
         start_with_comment: bool,
     ) -> bool {
-        self.highlighting = Vec::new();
         let chars: Vec<char> = self.string.chars().collect();
+        if self.is_highlighted && word.is_none() {
+            if let Some(hl_type) = self.highlighting.last() {
+                if *hl_type == highlighting::Type::MultilineComment
+                    && self.string.len() > 1
+                    && self.string[self.string.len() - 2..] == *"*/"
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        self.highlighting = Vec::new();
+
         let mut index = 0;
         let mut in_ml_comment = start_with_comment;
 
@@ -483,6 +499,7 @@ impl Row {
             return true;
         }
 
+        self.is_highlighted = true;
         false
     }
 
